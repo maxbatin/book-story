4f0d8d40ff30ac81e59fde83a12a1d987aa72e47
Max batin <batin@optimabank.kg>
2018-10-23 15:15:00 +0600

    init

---
+++ src/main/java/com/demo/bookstrory/BookStroryApplication.java
@@ -0,0 +1,12 @@
+package com.demo.bookstrory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStroryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStroryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/demo/bookstrory/Test.java
@@ -0,0 +1,5 @@
+package com.demo.bookstrory;
+
+public class Test {
+
+}
